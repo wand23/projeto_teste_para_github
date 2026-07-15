a248c8baef46dc76b6879429580f759f97fc5125
Wanderson <wanderson_-8998@hotmail.com>
2026-07-15 14:08:26 -0300

    adding functionality (taks inside a dict)

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
-import argparse
+import argparse, json
+from datetime import datetime
+
+parser = argparse.ArgumentParser(description="A simple command-line tool.")
+
+parser.add_argument("id", type=int)
+parser.add_argument("task", type=str)
+parser.add_argument("status", type=str)
+
+args = parser.parse_args()
+
+
+createdAT = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+updatedAt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+task_dict= {
+  "id": args.id,
+  "task": args.task,
+  "status": args.status,
+  "createdAt":createdAT,
+  "updatedAt":updatedAt,
+}
+
+
+print(task_dict)
 
